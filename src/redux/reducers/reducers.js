314e8aef8d120a6b7e90cc7db3a863f52bf86239
Puzant <PBAKJEJIAN@hotmail.com>
2020-06-27 23:06:10 +0300

    create the reducers for the state mangement

---
+++ src/redux/reducers/reducers.js
@@ -0,0 +1,60 @@
+import * as actions from '../actionTypes'
+
+const initialState = {
+  pending: false,
+  movies: [],
+  movie: [],
+  searchResults: [],
+  error: false
+}
+
+export default function moviesReducer(state = initialState, action) {
+  switch(action.type) {
+    case actions.GET_MOVIES_PENDING:
+      return {
+        ...state,
+        pending: true
+      }
+    case actions.GET_MOVIES_SUCCESS:
+      return {
+        ...state,
+        pending: false,
+        movies: action.payload.movies
+      }
+    case actions.GET_MOVIES_ERROR:
+      return {
+        ...state,
+        pending: false,
+        error: action.error
+      }
+    case actions.FILTER_MOVIES:
+      let filterMovies =  action.payload.movies.filter(function(movie) {
+        return movie.vote_average > 5.9;
+      }) 
+      return {
+        ...state,
+        movies: filterMovies
+      }
+    case actions.SORT_MOVIES:
+      let sortedMovies = action.payload.movies.sort(function(a,b){
+        return new Date(b.release_date) - new Date(a.release_date)
+      })
+      return {
+        ...state,
+        movies: sortedMovies
+      }
+    case actions.GET_MOVIE_SUCCESS: 
+      return {
+        ...state,
+        movie: action.payload.movie
+      }
+    case actions.GET_MOVIE_BY_QUERY_SUCCESS:
+      return {
+        ...state,
+        searchResults: action.payload.searchResults.results
+      }
+    default: 
+      return state
+  }
+
+}
