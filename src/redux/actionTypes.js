c546d367886da4fc20766d6604d2e4871ac46760
Puzant <PBAKJEJIAN@hotmail.com>
2020-06-27 23:06:41 +0300

    save action types in a seperate file

---
+++ src/redux/actionTypes.js
@@ -0,0 +1,8 @@
+export const GET_MOVIES_PENDING = "getMoviesPending"
+export const GET_MOVIES_SUCCESS = "getMoviesSuccess"
+export const GET_MOVIES_ERROR = "getMoviesError"
+export const FILTER_MOVIES = "filterMovies"
+export const SORT_MOVIES = "sortMovies"
+export const GET_MOVIE_SUCCESS = "getMovieSuccess"
+export const GET_MOVIE_BY_QUERY_SUCCESS = "getMovieByQuerySuccess"
+
