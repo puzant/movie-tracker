4508fcbfba23998de370eed00716d1cc525d0e49
Puzant <PBAKJEJIAN@hotmail.com>
2020-06-27 23:09:19 +0300

    root entry of the application
    create a new routes using react-router

--- src/App.js
+++ src/App.js
@@ -21,6 +21,64 @@ function App() {
       </header>
     </div>
   );
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import css from './App.css'
+import DiscoverMovies from './components/DiscoverMovies'
+import MovieOverview from './components/MovieOverview'
+import SearchResults from './components/SearchResults'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this);
+  }  
+
+  state = {
+    name: ''
+  }
+
+  handleChange(e) {
+    // console.log(this.state.name)
+    this.setState({ name: e.target.value })
+  }
+
+  render() {
+
+    return (
+      <Router>
+         <div className="main-app-container">
+           <div className="main-app-navbar">
+             <div className="navigation-items">
+               <li className="nav-item">
+                 <Link to="/">Discover Movies</Link>
+               </li>
+             </div>
+  
+             <div className="movies-search-bar">
+               <input onChange={this.handleChange} className="input-search-bar" type="text" placeholder="Enter Movie Name" />
+               <Link to={{pathname: 'search-results', search:`?search=${this.state.name}`}}>
+                <button onClick={this.handleRedirect} className="btn-search">Search</button>
+               </Link>
+             </div>
+           </div>
+        
+          <Switch>
+            <Route path="/" exact component={DiscoverMovies} />
+            <Route path="/movie-overview/:movieId" exact component={MovieOverview} />
+            <Route path="/search-results/" exact component={SearchResults} />
+          </Switch>
+        </div>
+      </Router>
+
+    );
+  }
 }
 
-export default App;
+export default App
