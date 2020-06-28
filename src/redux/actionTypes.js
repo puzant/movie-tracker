8adf96dae63ee1df0c75ac4316b84a3557d74f4e
Puzant <PBAKJEJIAN@hotmail.com>
2020-06-28 13:11:30 +0300

    add new action types (for filters)

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -5,4 +5,6 @@ export const FILTER_MOVIES = "filterMovies"
 export const SORT_MOVIES = "sortMovies"
 export const GET_MOVIE_SUCCESS = "getMovieSuccess"
 export const GET_MOVIE_BY_QUERY_SUCCESS = "getMovieByQuerySuccess"
+export const BY_HIGHEST_ORDER = "highestRating"
+export const BY_ADULT = "adult"
 
