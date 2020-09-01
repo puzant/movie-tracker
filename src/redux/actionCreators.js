99ede89044cae055d0ac03786e527ae215e912f2
Puzant <PBAKJEJIAN@hotmail.com>
2020-09-01 12:24:42 +0300

    refactor actionCreators

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,72 +1,148 @@
+import { createAction } from 'redux-actions';
 import * as actions from './actionTypes'
 import axios from 'axios';
 
-export function fetchMoviesPending() {
-  return {
-    type: actions.GET_MOVIES_PENDING
-  }
+/*
+ |--------------------------------------------------------------------------
+ | Get Movies
+ |--------------------------------------------------------------------------
+ */
+
+const fetchMoviesPending = createAction(actions.GET_MOVIES_PENDING)
+const fetchMoviesSuccess = createAction(actions.GET_MOVIES_SUCCESS, (movies) => ({movies}))
+const fetchMoviesError = createAction(actions.GET_MOVIES_ERROR, (error) => ({error}))
+
+export function fetchMovies() {
+  return dispatch => {
+    dispatch(fetchMoviesPending());
+    return axios
+      .get("https://api.themoviedb.org/3/discover/movie?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
+        .then((response) => {
+          return dispatch(fetchMoviesSuccess(response.data.results))
+        }).catch((error) => {
+          return dispatch(fetchMoviesError(error))
+        })
+    };
 }
 
-export function fetcMorehMoviesPending() {
-  return {
-    type: actions.GET_MORE_MOVIES_PENDING
-  }
-}
+/*
+ |--------------------------------------------------------------------------
+ | Get More Movies
+ |--------------------------------------------------------------------------
+ */
 
-export function fetchMoviesSuccess(movies) {
-  return {
-    type: actions.GET_MOVIES_SUCCESS,
-    payload: {
-      movies: movies
+ const fetcMorehMoviesPending = createAction(actions.GET_MORE_MOVIES_PENDING)
+ const fetchMoreMoviesSuccess = createAction(actions.GET_MORE_MOVIES_SUCCESS, (movies) => ({movies}))
+
+ export function fetchMoreMovies(pageNumber) {
+  return dispatch => {
+    dispatch(fetcMorehMoviesPending())
+    return axios
+      .get(`https://api.themoviedb.org/3/discover/movie?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber}`)
+        .then((response) => {
+          dispatch(fetchMoreMoviesSuccess(response.data.results))
+        })
+        .then((error) => {
+          dispatch(fetchMoviesError(error))
+        })
     }
-  }
 }
 
-export function fetchMoreMoviesSuccess(movies) {
-  return {
-    type: actions.GET_MORE_MOVIES_SUCCESS,
-    payload: {
-      movies: movies
-    }
-  }
-}
+/*
+ |--------------------------------------------------------------------------
+ | Get Movie
+ |--------------------------------------------------------------------------
+ */
 
-export function fetchMovieSuccess(movie) {
-  return {
-    type: actions.GET_MOVIE_SUCCESS,
-    payload: {
-      movie: movie
-    }
-  }
-}
+ const fetchMovieSuccess = createAction(actions.GET_MOVIE_SUCCESS, (movie) => ({movie}))
 
-export function fetchMovieByQuerySucess(searchResults) {
-  return  {
-    type: actions.GET_MOVIE_BY_QUERY_SUCCESS,
-    payload: {
-      searchResults: searchResults
-    }
+ export function fetchMovieById(movieId) {
+  return dispatch => {
+    return axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US`)
+      .then((response) => {
+        dispatch(fetchMovieSuccess(response.data))
+      })
+      .then((error) => {
+        dispatch(fetchMoviesError(error))
+      })
   }
 }
 
 
-export function fetchMoviesError(error) {
-  return {
-    type: actions.GET_MOVIES_ERROR, 
-    payload: {
-      error: error
-    }
+/*
+ |--------------------------------------------------------------------------
+ | Get Movie By Query
+ |--------------------------------------------------------------------------
+ */
+
+const fetchMovieByQuerySucess = createAction(actions.GET_MOVIE_BY_QUERY_SUCCESS, (searchResults) => ({searchResults}))
+
+export function fetchMovieByQuery(query) {
+  return dispatch => {
+    return axios.get(`https://api.themoviedb.org/3/search/movie?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US&query=${query}&page=1&include_adult=false`)
+      .then((response) => {
+        dispatch(fetchMovieByQuerySucess(response.data))
+      })
+      .then((error) => {
+        dispatch(fetchMoviesError(error))
+      })
   }
 }
 
-export function fetchMovieReviewsSuccess(reviews) {
-  return {
-    type: actions.GET_MOVIE_REVIEWS_SUCCESS,
-    payload: {
-      reviews: reviews
-    }
+/*
+ |--------------------------------------------------------------------------
+ | Get Movies Genres
+ |--------------------------------------------------------------------------
+ */
+
+const fetchMoviesGenresSuccess = createAction(actions.GET_GENRES, (genres) => ({genres}))
+
+export function fetchMoviesGenres() {
+  return dispatch => {
+    return axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US")
+      .then((response) => {
+        dispatch(fetchMoviesGenresSuccess(response.data.genres))
+      })
   }
 }
+
+/*
+ |--------------------------------------------------------------------------
+ | Get Movies Reviews
+ |--------------------------------------------------------------------------
+ */
+
+const fetchMovieReviewsSuccess = createAction(actions.GET_MOVIE_REVIEWS_SUCCESS, (reviews) => ({reviews}))
+
+export function fetchMovieReviews(movieId) {
+  return dispatch => {
+    return axios.get(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US&page=1`)
+      .then((response) => {
+        dispatch(fetchMovieReviewsSuccess(response.data.results))
+      })
+  }
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | Get Upcoming Movies
+ |--------------------------------------------------------------------------
+ */
+
+export function fetchUpcomingMovies() {
+  return dispatch => {
+    return axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US&page=1")
+      .then((response) => {
+        //  TODO: create action for the upcoming movies
+      })
+  }
+}
+
+
+// const filterMovies = createAction(actions.FILTER_MOVIES, (movies, filterType) => ({movies, filterType}))
+// const sortMovies = createAction(actions.SORT_MOVIES, (movies) => ({movies}))
+// const filterMoviesBasedByGenres = createAction(actions.FILTER_BASED_ON_GENRES, (genersId) => ({genersId}))
+
 
 export function filterMovies(movies, filterType) {
   return {
@@ -87,61 +163,12 @@ export function sortMovies(movies) {
   }
 }
 
-export function fetchMovies() {
-  return dispatch => {
-    dispatch(fetchMoviesPending());
-    return axios
-      .get("https://api.themoviedb.org/3/discover/movie?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
-        .then(function(response) {
-          dispatch(fetchMoviesSuccess(response.data.results))
-        })
-    };
-}
 
-export function fetchMoreMovies(pageNumber) {
-  return dispatch => {
-    dispatch(fetcMorehMoviesPending())
-    return axios
-      .get(`https://api.themoviedb.org/3/discover/movie?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber}`)
-        .then(function(response) {
-          dispatch(fetchMoreMoviesSuccess(response.data.results))
-        })
-        .then(function(error) {
-          dispatch(fetchMoviesError(error))
-        })
+export function filterMoviesBasedByGenres(genersId) {
+  return {
+    type: actions.FILTER_BASED_ON_GENRES,
+    payload: {
+      genersId: genersId
     }
-}
-
-
-export function fetchMovieById(movieId) {
-  return dispatch => {
-    return axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US`)
-      .then(function(response) {
-        dispatch(fetchMovieSuccess(response.data))
-      })
-      .then(function(error) {
-        dispatch(fetchMoviesError(error))
-      })
-  }
-}
-
-export function fetchMovieByQuery(query) {
-  return dispatch => {
-    return axios.get(`https://api.themoviedb.org/3/search/movie?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US&query=${query}&page=1&include_adult=false`)
-      .then(function(response) {
-        dispatch(fetchMovieByQuerySucess(response.data))
-      })
-      .then(function(error) {
-        dispatch(fetchMoviesError(error))
-      })
-  }
-}
-
-export function fetchMovieReviews(movieId) {
-  return dispatch => {
-    return axios.get(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=63d59f2df02d27e6739533218ba6c9d9&language=en-US&page=1`)
-      .then(function(response) {
-        dispatch(fetchMovieReviewsSuccess(response.data.results))
-      })
   }
 }
