e70e802c93bd04a1b4072ac28bffc027dfc2cf40
Puzant <PBAKJEJIAN@hotmail.com>
2020-09-01 12:26:05 +0300

    move to a nwe folder

---
+++ src/components/Movie/Movie.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+  const MoviePoster = (props) => (
+    <div className="movie-poster">
+      <img 
+        src={props.poster 
+          ? 
+            "http://image.tmdb.org/t/p/w185/" + props.poster 
+          : 
+            "https://cinemaone.net/images/movie_placeholder.png"} alt=""/>
+    </div>
+  )
+
+  const Movie = (props) => {
+    return (
+        <div className="movie-container">
+    
+          <MoviePoster poster={props.movie.poster_path} />
+    
+          <div className="movie-title-rating">
+            <div className="movie-title">{props.movie.title}</div>
+              <div className="movie-rating">Rating: 
+                <span className="rating-value">{props.movie.vote_average}</span>
+              </div>
+            </div>
+    
+            <div className="movie-release-date">{props.movie.release_date}</div>      
+    
+          </div>
+    )
+  }
+
+export default Movie;
