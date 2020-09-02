812b82e83f951c41ed853d6200113162a8f0f1fe
Puzant <PBAKJEJIAN@hotmail.com>
2020-09-02 13:23:22 +0300

    reformat the file structure and add more comments

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,15 +1,28 @@
-export const GET_MOVIES_PENDING = "getMoviesPending"
-export const GET_MORE_MOVIES_PENDING = "getMoreMoviesPending"
+/** Actions for Movies */
 export const GET_MOVIES_SUCCESS = "getMoviesSuccess"
-export const GET_MORE_MOVIES_SUCCESS = "getMoreMovies"
+export const GET_MOVIES_PENDING = "getMoviesPending"
 export const GET_MOVIES_ERROR = "getMoviesError"
+
+/** Actions for Load More Movies */
+export const GET_MORE_MOVIES_SUCCESS = "getMoreMovies"
+export const GET_MORE_MOVIES_PENDING = "getMoreMoviesPending"
 export const GET_MORE_MOVIES_ERROR = "getMoreMoviesError"
-export const FILTER_MOVIES = "filterMovies"
-export const SORT_MOVIES = "sortMovies"
+
+/** Actions for Movie */
 export const GET_MOVIE_SUCCESS = "getMovieSuccess"
 export const GET_MOVIE_BY_QUERY_SUCCESS = "getMovieByQuerySuccess"
-export const BY_HIGHEST_ORDER = "highestRating"
-export const BY_ADULT = "adult"
 export const GET_MOVIE_REVIEWS_SUCCESS = "getMovieReviewsSuccess"
 export const GET_GENRES = "getGenres"
+
+/** Actions for Movies Filters */
+export const FILTER_MOVIES = "filterMovies"
+export const SORT_MOVIES = "sortMovies"
+export const BY_HIGHEST_ORDER = "highestRating"
 export const FILTER_BASED_ON_GENRES = "filterBasedOnGenres"
+export const BY_ADULT = "adult"
+
+/** Actions for Upcomning Movies */
+export const GET_UPCOMING_MOVIES_SUCCESS = "getUpcomingMoviesSuccess"
+export const GET_UPCOMING_MOVIES_PENDING = "getUpcomingMoviesPending"
+export const GET_UPCOMING_MOVIES_ERROR = "getUpcomingMoviesError"
+
