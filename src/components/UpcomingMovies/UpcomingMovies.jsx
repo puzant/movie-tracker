37de325a68fd2cffb008ae7740bce4eadfc082f9
Puzant <PBAKJEJIAN@hotmail.com>
2020-09-02 13:22:53 +0300

    render the movies in upcoming movies component

--- src/components/UpcomingMovies/UpcomingMovies.jsx
+++ src/components/UpcomingMovies/UpcomingMovies.jsx
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import * as actions from '../../redux/actions/actionCreators'
+import {bindActionCreators} from 'redux'
+import PropTypes from 'prop-types';
 import Movie from '../Movie/Movie'
-
+import { Link } from "react-router-dom";
+import './style.css'
 class UpComingMovies extends Component {
   
-  constructor(props) {
-    super(props)
-    this.state = {
-      UpComingMovies: []
-    }
-  }
-
   componentDidMount() {
-    
+    this.props.fetchUpcomingMovies()
   }
 
   render() { 
+    const { upcomingMovies } = this.props 
     return ( 
       <div className="upcoming-movies-container">
-        {/* <Movie /> */}
+        {upcomingMovies && upcomingMovies.map((ucm) => (
+          <Link to={`/movie-overview/${ucm.id}`} key={ucm.id}>
+            <Movie movie={ucm} key={ucm.id} />
+          </Link>
+        ))}
       </div>
      );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    upcomingMovies: state.upcomingMovies
+  }
+}
  
-export default UpComingMovies;
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actions, dispatch)
+}
+
+UpComingMovies.propTypes = {
+  upcomingMovies: PropTypes.array,
+  fetchUpcomingMovies: PropTypes.func
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UpComingMovies);
