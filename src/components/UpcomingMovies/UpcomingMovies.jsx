ccfd3f9b0e526716c46cb56c05922048b6c612cf
Puzant <PBAKJEJIAN@hotmail.com>
2020-09-04 20:18:59 +0300

    add new prop types & render movies in upcoming movies comp

--- src/components/UpcomingMovies/UpcomingMovies.jsx
+++ src/components/UpcomingMovies/UpcomingMovies.jsx
@@ -6,6 +6,9 @@ import PropTypes from 'prop-types';
 import Movie from '../Movie/Movie'
 import { Link } from "react-router-dom";
 import './style.css'
+import MoviesLoader from '../MoviesLoader/MoviesLoader'
+import MoviesError from '../MoviesError/MoviesError'
+
 class UpComingMovies extends Component {
   
   componentDidMount() {
@@ -14,21 +17,34 @@ class UpComingMovies extends Component {
 
   render() { 
     const { upcomingMovies } = this.props 
-    return ( 
-      <div className="upcoming-movies-container">
-        {upcomingMovies && upcomingMovies.map((ucm) => (
-          <Link to={`/movie-overview/${ucm.id}`} key={ucm.id}>
-            <Movie movie={ucm} key={ucm.id} />
-          </Link>
-        ))}
+    return (
+      
+      <div className="upcoming-movies-main-container">
+        
+        <div className="upcoming-movies-sub-container">
+          {upcomingMovies && upcomingMovies.map((ucm) => (
+            <Link to={`/movie-overview/${ucm.id}`} key={ucm.id}>
+              <Movie movie={ucm} key={ucm.id}>
+              </Movie>
+            </Link>
+          ))}
+        </div>
+
+        <MoviesLoader pendingState={this.props.upcomingMoviesPending} />
+
+        <MoviesError error={this.props.upcomingMoviesError} />
+      
       </div>
+
      );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    upcomingMovies: state.upcomingMovies
+    upcomingMovies: state.upcomingMovies,
+    upcomingMoviesPending: state.upcomingMoviesPending,
+    upcomingMoviesError: state.upcomingMoviesError
   }
 }
  
@@ -38,7 +54,9 @@ const mapDispatchToProps = (dispatch) => {
 
 UpComingMovies.propTypes = {
   upcomingMovies: PropTypes.array,
-  fetchUpcomingMovies: PropTypes.func
+  fetchUpcomingMovies: PropTypes.func,
+  upcomingMoviesError: PropTypes.bool,
+  upcomingMoviesPending: PropTypes.bool
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(UpComingMovies);
