6a1461b58b42396221f44cbbad991c263a4f72bb
Puzant <PBAKJEJIAN@hotmail.com>
2020-09-04 20:18:23 +0300

    make some changes in navbar comp
    - render search bar in / only
    - add new error page route

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,6 @@ import {
   Switch,
   Route,
   NavLink,
-  Redirect
 } from "react-router-dom";
 
 import '../../assets/App.css'
@@ -12,22 +11,36 @@ import DiscoverMovies from '../DiscoverMovies/DiscoverMovies'
 import MovieOverview from '../MoviesOverview/MoviesOverview'
 import SearchResults from '../SearchResults/SearchResults'
 import UpcomingMovies from '../UpcomingMovies/UpcomingMovies'
+import ErrorPage from '../ErrorPage/ErrorPage'
 
 class NavBar extends Component {
 
   constructor(props) {
     super(props)
     this.handleChange = this.handleChange.bind(this);
+    this.resetState = this.resetState.bind(this)
     this.state = {
-      name: ""
+      name: "",
     }
-  }  
+  } 
 
   handleChange(e) {
     this.setState({ name: e.target.value })
   }
 
+  resetState() {
+    this.setState({ name: "" })
+  }
+
+  shouldRednerSearchBar() {
+    
+  }
+
   render() { 
+
+    const { shouldRender } = this.state
+
+    const notFoundPage = () => <ErrorPage errorText="Page Not Found"/>
 
     const NavItem = () => (
       <div className="navigation-items">
@@ -41,6 +54,12 @@ class NavBar extends Component {
       </div>
     )
 
+    const Login = () => (
+      <div className="login-container">
+        <span>Login</span>
+      </div>
+    )
+
     return ( 
       <Router>
          <div className="main-app-container">
@@ -48,12 +67,23 @@ class NavBar extends Component {
 
              <NavItem /> 
   
-             <div className="movies-search-bar">
-               <input value={this.state.name} onChange={this.handleChange} className="input-search-bar" type="text" placeholder="Enter Movie Name" />
-               <NavLink to={{pathname: 'search-results', search:`?search=${this.state.name}`}}>
-                <button className="btn-search">Search</button>
-               </NavLink>
+             <div className="sub-container">
+              {
+               window.location.pathname == '/' ?
+                <div className="movies-search-bar">
+                  <input value={this.state.name} onChange={this.handleChange} className="input-search-bar" type="text" placeholder="Enter Movie Name" />
+                  <NavLink to={{pathname: 'search-results', search:`?search=${this.state.name}`}}>
+                    <button onClick={this.resetState} className="btn-search">Search</button>
+                  </NavLink>
+                </div>
+                : 
+                null
+              }
+
+             {/* <Login /> */}
+             
              </div>
+             
            </div>
         
           <Switch>
@@ -61,8 +91,7 @@ class NavBar extends Component {
             <Route path="/movie-overview/:movieId" exact component={MovieOverview} />
             <Route path="/search-results" exact component={SearchResults} />
             <Route path="/upcoming-movies" exact component={UpcomingMovies} />
-            {/* Anything that dosen't match the above routes, redirect to / */}
-            <Redirect from='*' to='/' />
+            <Route component={notFoundPage} />
           </Switch>
         </div>
       </Router>
