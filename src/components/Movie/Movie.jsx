26d332e3b1092923c2f40a89aa83dbbf8c1334e9
Puzant <PBAKJEJIAN@hotmail.com>
2020-10-03 19:14:44 +0300

    add a rating by starts component

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
 
   const MoviePoster = (props = {}) => (
     <div className="movie-poster">
@@ -19,12 +21,24 @@ import React from 'react';
     
           <div className="movie-title-rating">
             <div className="movie-title">{props.movie.title}</div>
-              <div className="movie-rating">Rating: 
-                <span className="rating-value">{props.movie.vote_average}</span>
+              <div className="movie-rating">
+                <div className="movie-release-date">{props.movie.release_date}</div>      
+                {props.movie.vote_count &&
+                  <Box display='flex' alignItems='center'>
+                    <Rating
+                      style={{marginTop: '5px'}}
+                      value={props.movie.vote_average / 2}
+                      precision={0.5}
+                      readOnly
+                    />
+                    <Box component='span' ml={0.75} fontWeight="fontWeightMedium" fontSize="h6.fontSize">
+                      {props.movie.vote_average / 2}
+                    </Box>
+                  </Box>
+                }
               </div>
             </div>
     
-            <div className="movie-release-date">{props.movie.release_date}</div>      
     
           </div>
     )
