51ee723183bdec06a69cf9ddd288b7ee764ec64b
Puzant <PBAKJEJIAN@hotmail.com>
2020-10-03 22:54:51 +0300

    use .map to diisplay all the navLinks instead hard-coding it

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import Routes from '../../Root/Routes'
 import '../../assets/App.css'
 import { NavLink, BrowserRouter as Router } from "react-router-dom";
+import Constants from '../../constants/Constants'
 
 class NavBar extends Component {
 
   constructor(props) {
     super(props)
     this.handleChange = this.handleChange.bind(this);
-    this.resetState = this.resetState.bind(this)
     this.state = {
       name: "",
     }
@@ -18,21 +18,17 @@ class NavBar extends Component {
     this.setState({ name: e.target.value })
   }
 
-  resetState() {
-    this.setState({ name: "" })
-  }
-
   render() { 
 
     const NavItem = () => (
       <div className="navigation-items">
-        <li className="nav-item">
-        <NavLink exact activeStyle={{ fontWeight: "bold", color: "#111"}} to="/">Discover Movies</NavLink>
-        </li>
 
-        <li className="nav-item">
-          <NavLink exact activeStyle={{fontWeight: "bold", color: "#111"}} to="/upcoming-movies">Upcoming Movies</NavLink>
+        {Constants.NAVBAR_ITEMS.map((item) => (
+          <li className="nav-item">
+          <NavLink exact activeStyle={{fontWeight: "bold", color: "#82e0f5"}} to={item.routePath}>{item.navItemName}</NavLink>
         </li>
+        ))}
+        
       </div>
     )
 
@@ -40,18 +36,14 @@ class NavBar extends Component {
       <Router>
          <div className="main-app-container">
            <div className="main-app-navbar">
-
-             <NavItem /> 
-  
-              {
-               window.location.pathname == '/' &&
+             <NavItem />
+              {window.location.pathname == '/' &&
                 <div className="movies-search-bar">
                   <input value={this.state.name} onChange={this.handleChange} className="input-search-bar" type="text" placeholder="Enter Movie Name" />
                   <NavLink to={{pathname: 'search-results', search:`?search=${this.state.name}`}}>
-                    <button onClick={this.resetState} className="btn-search">Search</button>
+                    <button className="btn-search">Search</button>
                   </NavLink>
-                </div>
-              }            
+                </div>}            
              
            </div>
          </div>
