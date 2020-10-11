19e158715a3f7d7b431dbbf0e4ad971630f54651
Puzant <PBAKJEJIAN@hotmail.com>
2020-10-11 13:24:16 +0300

    add sorting action types

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -16,13 +16,55 @@ export const GET_GENRES = "getGenres"
 
 /** Actions for Movies Filters */
 export const FILTER_MOVIES = "filterMovies"
-export const SORT_MOVIES = "sortMovies"
 export const BY_HIGHEST_ORDER = "highestRating"
 export const FILTER_BASED_ON_GENRES = "filterBasedOnGenres"
 export const BY_ADULT = "adult"
+
+/** Actions for Movies Sorting */
+export const SORT_MOVIES = "sortMovies"
+export const SORT_BY_POPULARITY_DESCENDING = "popularityDescending"
+export const SORT_BY_POPULARITY_ASCENDING = "popularityAscending"
+export const SORT_BY_RATING_DESCENDING = "ratingDescending"
+export const SORT_BY_RATING_ASCENDING = "ratingAscending"
+export const SORT_BY_RELEASE_DATE_DESCENDING = "releaseDateDescending"
+export const SORT_BY_RELEASE_DATE_ASCENDING = "releaseDateAscending"
+
 
 /** Actions for Upcomning Movies */
 export const GET_UPCOMING_MOVIES_SUCCESS = "getUpcomingMoviesSuccess"
 export const GET_UPCOMING_MOVIES_PENDING = "getUpcomingMoviesPending"
 export const GET_UPCOMING_MOVIES_ERROR = "getUpcomingMoviesError"
 
+//  TODO: use this instead
+// export default {
+//   GET_MOVIES_ACTIONS: {
+//     GET_MOVIES_SUCCESS: "getMoviesSuccess",
+//     GET_MOVIES_PENDING: "getMoviesPending",
+//     GET_MOVIES_ERROR: "getMoviesError"
+//   },
+//   GET_MORE_MOVIES_ACTIONS: {
+//     GET_MORE_MOVIES_SUCCESS: "getMoreMovies",
+//     GET_MORE_MOVIES_PENDING: "getMoreMoviesPending",
+//     GET_MORE_MOVIES_ERROR: "getMoreMoviesError"
+//   },
+//   GET_MOVIE_ACTIONS: {
+//     GET_MOVIE_SUCCESS: "getMovieSuccess",
+//     GET_MOVIE_BY_QUERY_SUCCESS: "getMovieByQuerySuccess",
+//     GET_MOVIE_REVIEWS_SUCCESS: "getMovieReviewsSuccess",
+//     GET_GENRES: "getGenres"
+//   },
+//   GET_UPCOMING_MOVIES_ACTIONS: {
+//     GET_UPCOMING_MOVIES_SUCCESS: "getUpcomingMoviesSuccess",
+//     GET_UPCOMING_MOVIES_PENDING: "getUpcomingMoviesPending",
+//     GET_UPCOMING_MOVIES_ERROR: "getUpcomingMoviesError"
+//   },
+//   FILTER_MOVIES_ACTIONS: {
+//     FILTER_MOVIES: "filterMovies",
+//     BY_HIGHEST_ORDER: "highestRating",
+//     FILTER_BASED_ON_GENRES: "filterBasedOnGenres",
+//     BY_ADULT: "adult"
+//   },
+//   SORING_MOVIES_ACTIONS: {
+//     SORT_MOVIES: "sortMovies"
+//   }
+// }
