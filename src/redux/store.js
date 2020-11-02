45d530ec75bd7f11c91b418c0479791c1f70ae84
Puzant <PBAKJEJIAN@hotmail.com>
2020-11-02 20:45:51 +0200

    add dev tools in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension'
 import reducers from "./reducers/reducers";
 import thunk from "redux-thunk";
 
 const middlewares = [thunk];
-const store = createStore(reducers, applyMiddleware(...middlewares))
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(...middlewares)
+))
 
 export default store
