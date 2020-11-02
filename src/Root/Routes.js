497b917500cf95a357cce74d68b4967f824191d5
Puzant <PBAKJEJIAN@hotmail.com>
2020-11-02 17:59:35 +0200

    add new login route

--- src/Root/Routes.js
+++ src/Root/Routes.js
@@ -8,6 +8,7 @@ import DiscoverMovies from '../components/DiscoverMovies/DiscoverMovies'
 import MovieOverview from '../components/MoviesOverview/MoviesOverview'
 import SearchResults from '../components/SearchResults/SearchResults'
 import UpcomingMovies from '../components//UpcomingMovies/UpcomingMovies'
+import Login from '../components/Login/Login'
 import ErrorPage from '../components/ErrorPage/ErrorPage'
 import GuardedRoute from './GuardedRoute'
 
@@ -21,6 +22,7 @@ const Routes = () => {
       <Route path="/movie-overview/:movieId" exact component={MovieOverview} />
       <Route path="/search-results" exact component={SearchResults} />
       <Route path="/upcoming-movies" exact component={UpcomingMovies} />
+      <Route path ="/login" exact component={Login} />
       <GuardedRoute path="/profile" exact />
       <Route component={notFoundPage} />
   </Switch>
