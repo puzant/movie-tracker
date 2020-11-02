e93b8ca92b165868b7c66d0044a5d95b4387f95b
Puzant <PBAKJEJIAN@hotmail.com>
2020-11-02 20:44:48 +0200

    add new elements to the drawer

--- src/components/NavBar/MenuDrawer.jsx
+++ src/components/NavBar/MenuDrawer.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
+import MenuItem from '@material-ui/core/MenuItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Constants from '../../constants/Constants'
-import { NavLink, BrowserRouter as Router } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import menuIcon from '../../assets/menu-icon.jpg'
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 const useStyles = makeStyles({
   list: {
@@ -19,7 +23,14 @@ const useStyles = makeStyles({
   },
   menuIcon: {
     height: '40px'
-  }
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: 2,
+    minHeight: 64,
+  },
 });
 
 export default function MenuDrawer() {
@@ -41,13 +52,20 @@ export default function MenuDrawer() {
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
       >
+        <div className={classes.drawerHeader}>
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+      <Divider />
       <List>
         {Constants.NAVBAR_ITEMS.map((item, index) => (
-          <ListItem button key={index}>
-            <NavLink exact activeStyle={{fontWeight: "bold", color: "#00c6ff"}} to={item.routePath}>
+          <NavLink key={index} exact activeStyle={{fontWeight: "bold", color: "#00c6ff"}} to={item.routePath}>
+            <MenuItem button>
+              <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.navItemName} />
-            </NavLink>
-          </ListItem>
+            </MenuItem>
+          </NavLink>
         ))}
       </List>
     </div>
