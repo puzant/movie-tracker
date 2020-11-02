187705b2a32e0be034e5ab849c060041ecfba24a
Puzant <PBAKJEJIAN@hotmail.com>
2020-11-02 20:42:38 +0200

    add new TMDB api endpoints

--- src/api/api.js
+++ src/api/api.js
@@ -8,11 +8,13 @@ const API_KEY = `63d59f2df02d27e6739533218ba6c9d9`
 
 const getMovies = () => api.get(`discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
 const getMoreMovies = (pageNumber) => api.get(`discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber}`)
-const getMovieById = (movieId) => api.get(`movie/${movieId}?api_key=${API_KEY}&language=en-US`)
+const getMovieById = (movieId) => api.get(`movie/${movieId}?append_to_response=credits,keyword,recommendations&api_key=${API_KEY}`)
 const getMovieByQuery = (queryParam) => api.get(`/search/movie?api_key=${API_KEY}&language=en-US&query=${queryParam}&page=1&include_adult=false`)
 const getMovieGenres = () => api.get(`genre/movie/list?api_key=${API_KEY}&language=en-US`)
 const getMovieReviews = (movieId) => api.get(`movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`)
 const getUpcomingMovies = () => api.get(`movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`)
+const getMovieImages = (movieId) => api.get(`/movie/${movieId}/images?api_key=${API_KEY}`)
+const getMovieRecommendations = (movieId) => api.get(`/movie/${movieId}/recommendations`)
 
 export default {
   getMovies,
@@ -21,5 +23,7 @@ export default {
   getMovieByQuery,
   getMovieGenres,
   getMovieReviews,
-  getUpcomingMovies
+  getUpcomingMovies,
+  getMovieImages,
+  getMovieRecommendations
 }
