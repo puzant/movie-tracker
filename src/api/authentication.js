6f76034ac11aa1cf22b2baef56bd79d2cf7c67c5
Puzant <PBAKJEJIAN@hotmail.com>
2020-11-09 22:53:27 +0200

    import new axios instnace and use it

--- src/api/authentication.js
+++ src/api/authentication.js
@@ -1,12 +1,21 @@
-import axios from 'axios'
+import axios from './axiosInstance'
 
 export const getRequestToken = () => axios.get(`/authentication/token/new`)
 
 export const login = (username, password, requestToken) => {
   axios.post('/authentication/token/validate_with_login', {
-    username,
-    password,
+    username: username,
+    password: password,
     request_token: requestToken
   })
 }
 
+export const createSession = (requestToken) => {
+  axios.post('/authentication/session/new', {
+    request_token: requestToken
+  })
+}
+
+export const deleteSession = (sessionId) => {
+  axios.delete('/authentication/session', {session_id: sessionId})
+}
