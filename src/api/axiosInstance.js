bd25967e218e8fcfc8c4b3e56698e0ecabf6e1cf
Puzant <pbakjejian@hotmail.com>
2020-12-15 12:31:22 +0200

    fix comment

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_TMDB_API_BASE_URL,
 });
 
-//  add the API_KEY with every request
+//  attach the API_KEY with every request
 axiosInstance.interceptors.request.use((config) => {
   config.params = {
     ...config.params,
