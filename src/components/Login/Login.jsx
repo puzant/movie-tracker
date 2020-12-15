e52c5a070bba3e3916ceb2e95ffcb4cc506ebc43
Puzant <pbakjejian@hotmail.com>
2020-12-15 12:31:59 +0200

    move all the css code to styled components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,107 @@
 import React, { Component } from 'react';
-import './style.css'
 import Info from '@material-ui/icons/Info'
 import Constants from '../../constants/Constants'
+import {bindActionCreators} from 'redux'
+import * as actions from '../../redux/actions/actionCreators.js';
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
+import styled from 'styled-components'
+
+/** Login Styles Go Here */
+const LoginContainer = styled.div`
+padding: 10px;
+margin: 40px auto;
+width: 80%;
+`
+
+const LoginText = styled.div`
+font-size: 1.5em;
+font-weight: 600;
+margin-bottom: 20px;
+`
+
+const NoticeText = styled.div`
+line-height: 20px;
+margin-bottom: 16px;
+`
+
+const LoginButtonContainer = styled.div`
+display: flex;
+justify-content: flex-star;
+margin-top: 18px;
+`
+
+const LoginButton = styled.div`
+border-color: #01b4e4;
+background-color: #01b4e4;
+color: #fff;
+padding: .675rem .95rem;
+border-radius: 5px;
+font-weight: bold;
+border-radius: 14px;
+&:hover {
+  cursor: pointer;
+}
+`
+
+const CreateNewAccountLink = styled.a`
+color: #00c6ff;
+`
+
+const LabelName = styled.div`
+font-weight: bold;
+margin-top: 15px;
+`
+
+const InputForm = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+const StyledInput = styled.input`
+margin-top: 10px;
+border-color: rgba(33,37,41,0.15);
+color: #292b2c;
+padding: 12px;
+border-radius: 1.25rem;
+line-height: 1.5;
+vertical-align: middle;
+&:focus {
+  outline: none;
+}
+`
+
+const ErrorCardContainer = styled.div`
+margin: 20px 0 10px 0;
+box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+background-color: #fff;
+border-radius: 8px;
+border: 1px solid #ccc;
+`
+
+const ErrorCardHeader = styled.div`
+background-color: #d53540;
+color: #fff;
+display: flex;
+padding: 20px;
+border-top-left-radius: 7px;
+border-top-right-radius: 7px;
+`
+
+const ErrorText = styled.div`
+font-weight: 600;
+font-size: 1.2em;
+line-height: 1.2em;
+margin-left: 5px;
+`
+
+const ErrorTypeContainer = styled.div`
+padding: 5px;
+`
+
+const StyledUnorderedList = styled.ul`
+line-height: 1.4;
+`
 
 class Login extends Component {
   constructor(props) {
@@ -12,7 +112,8 @@ class Login extends Component {
       username: null,
       password: null,
       validationErrorExsist: false,
-      validationErrorText: null
+      validationErrorText: null,
+      isAuthenticated: this.props.isAuth
     }
   }
 
@@ -31,62 +132,78 @@ class Login extends Component {
     this.setState({password: event.target.value})
   }
 
-  validateUserInput = () => {
+  login = () => {
     if((this.state.username == null || this.state.password == null)) {
       this.setState({validationErrorExsist: true}, () => {
-        this.GetErrorText()
+        return this.GetErrorText()
       })
+    }
+    else {
+      return this.props.loginUser(this.state.username, this.state.password)
     }
   }
 
   render() { 
+
+    if(this.state.isAuthenticated) return <Redirect to='/' />
         
     const {validationErrorExsist} = this.state
 
     const ErrorStatusCard = () => (
-      <div className="error-card-container">
-        <div className="error-card-header">
+      <ErrorCardContainer>
+        <ErrorCardHeader>
           <Info />
-          <div className="error-text">There was a problem!</div>
-        </div>
-        <div className="error-type-cont">
-          <ul>
+          <ErrorText>There was a problem!</ErrorText>
+        </ErrorCardHeader>
+        <ErrorTypeContainer>
+          <StyledUnorderedList>
             <li>{this.state.validationErrorText}</li>
             {/* TODO: login attemps */}
             {/* <li>You have 10 remaining login attempts.</li> */}
-          </ul>
-        </div>
-      </div>
+          </StyledUnorderedList>
+        </ErrorTypeContainer>
+      </ErrorCardContainer>
     )
 
     return ( 
-      <div className="login-container">
+      <LoginContainer>
 
-        <div className="login-text">Login to your account</div>
-        <div className="notice-text">
+        <LoginText>Login to your account</LoginText>
+        <NoticeText>
           This app gets its data from the TMDD APIs. To view your account information, login with your TMDb credentials in the form below. To create one, 
-           <a className="create-new-account-link" target="_blank" href="https://www.themoviedb.org/signup"> Click here</a>
-        </div>
+           <CreateNewAccountLink href="https://www.themoviedb.org/signup" target="_blank"> Click here</CreateNewAccountLink>
+        </NoticeText>
 
         {validationErrorExsist && <ErrorStatusCard />}
 
-        <div className="input-form">
-          <span className="label-name">Username</span>
-          <input onChange={this.handleUsernameInput} type="text"/>
-        </div>
+        <InputForm>
+          <LabelName>Username</LabelName>
+          <StyledInput onChange={this.handleUsernameInput} type="text"/>
+        </InputForm>
 
-        <div className="input-form">
-          <span className="label-name">Password</span>
-          <input onChange={this.handlePasswordInput} type="password"/>
-        </div>
+        <InputForm>
+          <LabelName>Password</LabelName>
+          <StyledInput onChange={this.handlePasswordInput} type="password"/>
+        </InputForm>
 
-        <div className="login-btn-container">
-          <div className="login-btn" onClick={this.validateUserInput}>Login</div>
-        </div>
+        <LoginButtonContainer>
+          {/* <div className="login-btn" onClick={this.login}>Login</div> */}
+          <LoginButton onClick={this.login}>Login</LoginButton>
+        </LoginButtonContainer>
 
-      </div> 
+      </LoginContainer> 
     );
   }
 }
+
+const mapStateToProps = (state) => { 
+  return {
+    isAuth: state.isAuth,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actions, dispatch)
+}
  
-export default Login;
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
