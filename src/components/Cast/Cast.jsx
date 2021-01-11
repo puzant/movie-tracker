93ca11077142aecd0ee451e82d846f92d30bd51c
Puzant <pbakjejian@hotmail.com>
2021-01-11 12:17:36 +0200

    remove unsed imports

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import UnknowActorImage from '../../assets/unknown-actor.jpg'
 
@@ -31,7 +28,7 @@ const Cast = (props) => {
             `https://image.tmdb.org/t/p/w185/${props.actor.profile_path}` 
             : UnknowActorImage
           }
-          title="Contemplative Reptile"
+          title={props.actor.name}
         />
         <CardContent>
           <Typography variant="h6">{props.actor.name}</Typography>
