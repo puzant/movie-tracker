6990152dec06b893503d1392c44723a7bb19ab2f
Puzant <pbakjejian@hotmail.com>
2021-01-15 16:19:16 +0200

    add return statement

--- src/api/authentication.js
+++ src/api/authentication.js
@@ -3,7 +3,7 @@ import axios from './axiosInstance'
  const getRequestToken = () => axios.get(`/authentication/token/new`)
 
  const login = (username, password, requestToken) => {
-  axios.post('/authentication/token/validate_with_login', {
+  return axios.post('/authentication/token/validate_with_login', {
     username: username,
     password: password,
     request_token: requestToken
@@ -11,17 +11,15 @@ import axios from './axiosInstance'
 }
 
  const createSession = (requestToken) => {
-  axios.post('/authentication/session/new', {
+  return axios.post('/authentication/session/new', {
     request_token: requestToken
   })
 }
 
-const getAccount = (sessionId) => {
-  axios.post(`/account?session_id=${sessionId}`)
-}
+const getAccount = (sessionId) => axios.get(`/account?session_id=${sessionId}`)
 
  const deleteSession = (sessionId) => {
-  axios.delete('/authentication/session', {session_id: sessionId})
+  return axios.delete('/authentication/session', {session_id: sessionId})
 }
 
 export default { getRequestToken, login, createSession, getAccount, deleteSession }
