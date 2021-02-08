588bad4832f61fdd97f864e94a43531e6c3d2ec2
Puzant <pbakjejian@hotmail.com>
2021-02-08 23:52:54 +0200

    combine all the reducers and add to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension'
-import reducers from "./reducers/reducers";
+import rootReducers from './reducers/rootReducers'
 import thunk from "redux-thunk";
 
 const middlewares = [thunk];
-const store = createStore(reducers, composeWithDevTools(
+const store = createStore(rootReducers, composeWithDevTools(
   applyMiddleware(...middlewares)
 ))
 
