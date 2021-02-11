2b50fc269f5e7f351410d63e96dfb6b4149ec50c
Puzant <pbakjejian@hotmail.com>
2021-02-11 20:34:09 +0200

    add alt prop

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import loader from '../../assets/loader-dotted.gif'
 
 const Loader = (props) => (
   <div className="movie-loader-container">
-      {props.pendingState && <img className="movies-loader" src={loader} />}
+      {props.pendingState && <img className="movies-loader" alt="loading icon" src={loader} />}
   </div>
 )
 
