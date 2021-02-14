b440079c29f7ade7e4f67ea395e2b2e79d1df8de
Puzant <pbakjejian@hotmail.com>
2021-02-14 22:47:27 +0200

    :rewind: remove transition router

--- src/Root/Routes.js
+++ src/Root/Routes.js
@@ -5,9 +5,8 @@ import MovieOverview from '../components/movieOverview/movieOverview'
 import SearchResults from '../components/searchResults/searchResults'
 import UpcomingMovies from '../components/upcomingMovies/upcomingMovies'
 import Login from '../components/login/login'
-import ErrorPage from '../components/ErrorPage/ErrorPage'
+import { ErrorPage } from '../components/errorPage/errorPage'
 import GuardedRoute from './guardedRoute'
-import { AnimatedSwitch } from 'react-router-transition';
 
 const notFoundPage = () => <ErrorPage errorText="Page Not Found"/>
 
@@ -15,20 +14,13 @@ const Routes = () => {
  
   return (
     <Switch>
-      <AnimatedSwitch
-        atEnter={{ opacity: 0 }}
-        atLeave={{ opacity: 0 }}
-        atActive={{ opacity: 1 }}
-        className="switch-wrapper"
-      >
-        <Route path="/" exact component={DiscoverMovies} />
-        <Route path="/movie-overview/:movieId" exact component={MovieOverview} />
-        <Route path="/search-results" exact component={SearchResults} />
-        <Route path="/upcoming-movies" exact component={UpcomingMovies} />
-        <Route path ="/login" exact component={Login} />
-        <GuardedRoute path="/profile" exact />
-        <Route component={notFoundPage} />
-      </AnimatedSwitch>   
+      <Route path="/" exact component={DiscoverMovies} />
+      <Route path="/movie-overview/:movieId" exact component={MovieOverview} />
+      <Route path="/search-results" exact component={SearchResults} />
+      <Route path="/upcoming-movies" exact component={UpcomingMovies} />
+      <Route path ="/login" exact component={Login} />
+      <GuardedRoute path="/profile" exact />
+      <Route component={notFoundPage} />
     </Switch>
   )
 }
