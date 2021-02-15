5acca6083e9f70ad7cb05790914b54149022f901
Puzant <pbakjejian@hotmail.com>
2021-02-15 21:57:48 +0200

    :art: move search movies action types to separate file

---
+++ src/redux/actionTypes/searchMoviesActionTypes.js
@@ -0,0 +1,7 @@
+export default {
+  SEARCH_MOVIES_ACTIONS: {
+    GET_MOVIE_BY_QUERY_SUCCESS: "getMovieByQuerySuccess",
+    GET_MOVIE_BY_QUERY_PENDING: "getMovieByQueryPending",
+    GET_MOVIE_BY_QUERY_ERROR: "getMovieByQueryError",
+  }
+}
