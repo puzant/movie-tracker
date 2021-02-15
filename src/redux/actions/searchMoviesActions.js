d8aed84f2d5005ce50a8438ee12278f8f496b836
Puzant <pbakjejian@hotmail.com>
2021-02-15 16:36:08 +0200

    :hammer:

---
+++ src/redux/actions/searchMoviesActions.js
@@ -0,0 +1,29 @@
+import { createAction } from 'redux-actions';
+import * as actions from './actionTypes'
+import api from '../../api/api'
+
+/*
+ |--------------------------------------------------------------------------
+ | Get Movie By Query
+ |--------------------------------------------------------------------------
+ */
+
+const fetchMovieByQuerySuccess = createAction(actions.GET_MOVIE_BY_QUERY_SUCCESS, (searchResults) => ({searchResults}))
+const fetchMovieByQueryPending = createAction(actions.GET_MOVIE_BY_QUERY_PENDING)
+const fetchMovieByQueryError = createAction(actions.GET_MOVIE_BY_QUERY_ERROR, (error) => ({error}))
+
+export const fetchMovieByQuery = (query) => {
+  return async dispatch => {
+    try {
+      dispatch(fetchMovieByQueryPending())
+      const movieByQueryResponse = await api.getMovieByQuery(query)
+      return dispatch(fetchMovieByQuerySuccess(movieByQueryResponse.data))
+    } catch(error) {
+      dispatch(fetchMovieByQueryError(error))
+    }
+  }
+}
+
+export default {
+  fetchMovieByQuery 
+}
