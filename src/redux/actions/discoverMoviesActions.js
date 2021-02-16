be829381d1182c813285b91c787cbce72e54aa69
Puzant <pbakjejian@hotmail.com>
2021-02-16 20:11:54 +0200

    :lipstick: change the methods

--- src/redux/actions/discoverMoviesActions.js
+++ src/redux/actions/discoverMoviesActions.js
@@ -1,5 +1,11 @@
 import { createAction } from 'redux-actions';
-import discoverMoviesActionTypes from '../actionTypes/discoverMoviesActionTypes'
+import { 
+    GET_MOVIES_ACTIONS,
+    GET_MORE_MOVIES_ACTIONS,
+    GET_MOVIES_GENRES,
+    FILTER_MOVIES_ACTIONS,
+    SORTING_MOVIES_ACTIONS 
+  } from '../actionTypes/discoverMoviesActionTypes'
 import api from '../../api/api'
 
 /*
@@ -8,9 +14,9 @@ import api from '../../api/api'
  |--------------------------------------------------------------------------
  */
 
-const fetchMoviesPending = createAction(discoverMoviesActionTypes.GET_MOVIES_ACTIONS.PENDING)
-const fetchMoviesSuccess = createAction(discoverMoviesActionTypes.GET_MOVIES_ACTIONS.SUCCESS, (movies) => ({movies}))
-const fetchMoviesError = createAction(discoverMoviesActionTypes.GET_MOVIES_ACTIONS.ERROR, (error) => ({error}))
+const fetchMoviesPending = createAction(GET_MOVIES_ACTIONS.PENDING)
+const fetchMoviesSuccess = createAction(GET_MOVIES_ACTIONS.SUCCESS, (movies) => ({movies}))
+const fetchMoviesError = createAction(GET_MOVIES_ACTIONS.ERROR, (error) => ({error}))
 
 const fetchMovies = () => {
   return async dispatch => {
@@ -31,7 +37,7 @@ const fetchMovies = () => {
  |--------------------------------------------------------------------------
  */
 
-const fetchMoviesGenresSuccess = createAction(discoverMoviesActionTypes.GET_MOVIES_GENRES, (genres) => ({genres}))
+const fetchMoviesGenresSuccess = createAction(GET_MOVIES_GENRES, (genres) => ({genres}))
 
 const fetchMoviesGenres = () => {
   return async dispatch => {
@@ -50,9 +56,9 @@ const fetchMoviesGenres = () => {
  |--------------------------------------------------------------------------
  */
 
-const fetcMorehMoviesPending = createAction(discoverMoviesActionTypes.GET_MORE_MOVIES_ACTIONS.PENDING)
-const fetchMoreMoviesSuccess = createAction(discoverMoviesActionTypes.GET_MORE_MOVIES_ACTIONS.SUCCESS, (movies) => ({movies}))
-const fetchMoreMoviesError = createAction(discoverMoviesActionTypes.GET_MORE_MOVIES_ACTIONS.ERROR, (error) => ({error}))
+const fetcMorehMoviesPending = createAction(GET_MORE_MOVIES_ACTIONS.PENDING)
+const fetchMoreMoviesSuccess = createAction(GET_MORE_MOVIES_ACTIONS.SUCCESS, (movies) => ({movies}))
+const fetchMoreMoviesError = createAction(GET_MORE_MOVIES_ACTIONS.ERROR, (error) => ({error}))
 
 const fetchMoreMovies = (pageNumber) => {
  return async dispatch => {
@@ -72,9 +78,9 @@ const fetchMoreMovies = (pageNumber) => {
  |--------------------------------------------------------------------------
  */
 
-const filter = createAction(discoverMoviesActionTypes.FILTER_MOVIES_ACTIONS.FILTER_MOVIES, (movies, filterType) => ({movies, filterType}))
-const sort = createAction(discoverMoviesActionTypes.SORTING_MOVIES_ACTIONS.SORT_MOVIES, (movies, sortingType) => ({movies, sortingType}))
-const filterByGenres = createAction(discoverMoviesActionTypes.FILTER_MOVIES_ACTIONS.FILTER_BASED_ON_GENRES, (genersId) => ({genersId}))
+const filter = createAction(FILTER_MOVIES_ACTIONS.FILTER_MOVIES, (movies, filterType) => ({movies, filterType}))
+const sort = createAction(SORTING_MOVIES_ACTIONS.SORT_MOVIES, (movies, sortingType) => ({movies, sortingType}))
+const filterByGenres = createAction(FILTER_MOVIES_ACTIONS.FILTER_BASED_ON_GENRES, (genersId) => ({genersId}))
 
 const filterMovies = (movies, filterType) => {
   return dispatch => {
