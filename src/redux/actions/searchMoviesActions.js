133a7a7c5b39e2e0744a990b998d4072245ac510
Puzant <pbakjejian@hotmail.com>
2021-03-09 11:48:55 +0200

    :bug: fix geners fetch action

--- src/redux/actions/searchMoviesActions.js
+++ src/redux/actions/searchMoviesActions.js
@@ -11,13 +11,18 @@ import api from '../../api/api'
 const fetchMovieByQuerySuccess = createAction(SEARCH_MOVIES_ACTIONS.SUCCESS, (searchResults) => ({searchResults}))
 const fetchMovieByQueryPending = createAction(SEARCH_MOVIES_ACTIONS.PENDING)
 const fetchMovieByQueryError = createAction(SEARCH_MOVIES_ACTIONS.ERROR, (error) => ({error}))
+const emptySearchResults = createAction(SEARCH_MOVIES_ACTIONS.EMPTY)
 
 const fetchMovieByQuery = (query) => {
   return async dispatch => {
     try {
       dispatch(fetchMovieByQueryPending())
       const movieByQueryResponse = await api.getMovieByQuery(query)
-      return dispatch(fetchMovieByQuerySuccess(movieByQueryResponse.data))
+      if (!movieByQueryResponse.data.results.length)
+        return dispatch(emptySearchResults())
+      
+      dispatch(fetchMovieByQuerySuccess(movieByQueryResponse.data))
+      
     } catch(error) {
       dispatch(fetchMovieByQueryError(error))
     }
