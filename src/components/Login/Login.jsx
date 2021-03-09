b3bee37cc6f80e2c913023cf776ecef19bc44330
Puzant <pbakjejian@hotmail.com>
2021-03-09 11:46:39 +0200

    :hammer: refactor to functional component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,199 +1,120 @@
-import React, { Component } from 'react';
-import Info from '@material-ui/icons/Info'
-import Constants from '../../constants/Constants'
+import React from 'react'
+import styled from 'styled-components'
+import Alert from '@material-ui/lab/Alert'
 import { bindActionCreators } from 'redux'
 import loginActions from '../../redux/actions/loginActions'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
-import styled from 'styled-components'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
-/** Login Styles Go Here */
-const LoginContainer = styled.div`
-padding: 10px;
-margin: 40px auto;
-width: 80%;
+export const Login = ({loginUser, loginSuccess, loginError}) => {
+
+  const [usernameValue, setUsernameValue] = React.useState('')
+  const [passwordValue, setPasswordValue] = React.useState('')
+
+  const handleUsernameValueChange = (e) => {
+    setUsernameValue(e.target.value)
+  }
+
+  const handlePasswordValueChange = (e) => {
+    setPasswordValue(e.target.value)
+  }
+
+  const handleUserLogin = () => {
+    loginUser(usernameValue, passwordValue)
+  }
+
+  return (
+    <Root>
+
+      <LoginText>Login to your account</LoginText>
+      <NoticeText>
+        This app gets its data from the TMDD APIs. To view your account information, login with your TMDb credentials in the form below. To create one, 
+        <CreateNewAccountLink href="https://www.themoviedb.org/signup" target="_blank"> Click here</CreateNewAccountLink>
+      </NoticeText>
+
+      {loginError && <Alert severity="error">Username & Password are required</Alert>}
+
+      <InputForm>
+        <LabelName>Username</LabelName>
+        <StyledInput onChange={handleUsernameValueChange} type="text"/>
+      </InputForm>
+
+      <InputForm>
+        <LabelName>Password</LabelName>
+        <StyledInput onChange={handlePasswordValueChange} type="password"/>
+      </InputForm>
+
+      <LoginButtonContainer>
+        <LoginButton onClick={handleUserLogin}>Login</LoginButton>
+      </LoginButtonContainer>
+
+    </Root>
+  )
+
+}
+
+const Root = styled.div`
+  padding: 10px;
+  margin: 40px auto;
+  width: 80%;
 `
 
 const LoginText = styled.div`
-font-size: 1.5em;
-font-weight: 600;
-margin-bottom: 20px;
+  font-size: 1.5em;
+  font-weight: 600;
+  margin-bottom: 20px;
 `
 
 const NoticeText = styled.div`
-line-height: 20px;
-margin-bottom: 16px;
+  line-height: 20px;
+  margin-bottom: 16px;
 `
 
 const LoginButtonContainer = styled.div`
-display: flex;
-justify-content: flex-star;
-margin-top: 18px;
+  display: flex;
+  justify-content: flex-star;
+  margin-top: 18px;
 `
 
 const LoginButton = styled.div`
-border-color: #01b4e4;
-background-color: #01b4e4;
-color: #fff;
-padding: .675rem .95rem;
-border-radius: 5px;
-font-weight: bold;
-border-radius: 14px;
+  border-color: #01b4e4;
+  background-color: #01b4e4;
+  color: #fff;
+  padding: .675rem .95rem;
+  border-radius: 5px;
+  font-weight: bold;
+  border-radius: 14px;
 &:hover {
   cursor: pointer;
 }
 `
 
 const CreateNewAccountLink = styled.a`
-color: #00c6ff;
+  color: #00c6ff;
 `
 
 const LabelName = styled.div`
-font-weight: bold;
-margin-top: 15px;
+  font-weight: bold;
+  margin-top: 15px;
 `
 
 const InputForm = styled.div`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `
 
 const StyledInput = styled.input`
-margin-top: 10px;
-border: 1px solid #abb3ba;
-color: #292b2c;
-padding: 12px;
-border-radius: 1.25rem;
-line-height: 1.5;
-vertical-align: middle;
+  margin-top: 10px;
+  border: 1px solid #abb3ba;
+  color: #292b2c;
+  padding: 12px;
+  border-radius: 1.25rem;
+  line-height: 1.5;
+  vertical-align: middle;
 &:focus {
   outline: none;
 }
 `
-
-const ErrorCardContainer = styled.div`
-margin: 20px 0 10px 0;
-background-color: #fff;
-border-radius: 8px;
-border: 1px solid #ccc;
-`
-
-const ErrorCardHeader = styled.div`
-background-color: #d53540;
-color: #fff;
-display: flex;
-padding: 20px;
-border-top-left-radius: 7px;
-border-top-right-radius: 7px;
-`
-
-const ErrorText = styled.div`
-font-weight: 600;
-font-size: 1.2em;
-line-height: 1.2em;
-margin-left: 5px;
-`
-
-const ErrorTypeContainer = styled.div`
-padding: 5px;
-`
-
-const StyledUnorderedList = styled.ul`
-line-height: 1.4;
-`
-
-class Login extends Component {
-  constructor(props) {
-    super(props)
-    this.handleUsernameInput = this.handleUsernameInput.bind(this)
-    this.handlePasswordInput = this.handlePasswordInput.bind(this)
-    this.state = {
-      username: null,
-      password: null,
-      validationErrorExsist: false,
-      validationErrorText: null,
-      isAuthenticated: this.props.isAuth
-    }
-  }
-
-  GetErrorText = () => {
-    //  username field is empty
-    if(this.state.username == null) this.setState({ validationErrorText: Constants.VALIDATION_TEXT.EMPTY_USERNAME_FEILD })
-    //  password field is empty
-    else if(this.state.password == null) this.setState({ validationErrorText: Constants.VALIDATION_TEXT.EMPTY_PASSOWRD_FEILD })
-  }
-
-  handleUsernameInput = (event) => {
-    this.setState({username: event.target.value})
-  }
-  
-  handlePasswordInput = (event) => {
-    this.setState({password: event.target.value})
-  }
-
-  login = () => {
-    if((this.state.username == null || this.state.password == null)) {
-      this.setState({validationErrorExsist: true}, () => {
-         this.GetErrorText()
-      })
-    }
-    else {
-       this.props.loginActions(this.state.username, this.state.password)
-    }
-  }
-
-  render() { 
-
-    if(this.state.isAuthenticated) return <Redirect to='/' />
-        
-    const {validationErrorExsist} = this.state
-
-    const ErrorStatusCard = () => (
-      <ErrorCardContainer>
-        <ErrorCardHeader>
-          <Info />
-          <ErrorText>There was a problem!</ErrorText>
-        </ErrorCardHeader>
-        <ErrorTypeContainer>
-          <StyledUnorderedList>
-            <li>{this.state.validationErrorText}</li>
-            {/* TODO: login attemps */}
-            {/* <li>You have 10 remaining login attempts.</li> */}
-          </StyledUnorderedList>
-        </ErrorTypeContainer>
-      </ErrorCardContainer>
-    )
-
-    return ( 
-      <LoginContainer>
-
-        <LoginText>Login to your account</LoginText>
-        <NoticeText>
-          This app gets its data from the TMDD APIs. To view your account information, login with your TMDb credentials in the form below. To create one, 
-           <CreateNewAccountLink href="https://www.themoviedb.org/signup" target="_blank"> Click here</CreateNewAccountLink>
-        </NoticeText>
-
-        {validationErrorExsist && <ErrorStatusCard />}
-
-        <InputForm>
-          <LabelName>Username</LabelName>
-          <StyledInput onChange={this.handleUsernameInput} type="text"/>
-        </InputForm>
-
-        <InputForm>
-          <LabelName>Password</LabelName>
-          <StyledInput onChange={this.handlePasswordInput} type="password"/>
-        </InputForm>
-
-        <LoginButtonContainer>
-          {/* <div className="login-btn" onClick={this.login}>Login</div> */}
-          <LoginButton onClick={this.login}>Login</LoginButton>
-        </LoginButtonContainer>
-
-      </LoginContainer> 
-    );
-  }
-}
 
 const mapStateToProps = (state) => { 
   return {
@@ -204,5 +125,5 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(loginActions, dispatch)
 }
- 
+
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
