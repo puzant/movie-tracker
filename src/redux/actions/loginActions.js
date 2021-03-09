2bde0781635b4a2546fdfbecd1a8f2c410edd092
Puzant <pbakjejian@hotmail.com>
2021-03-09 11:48:22 +0200

    :lipstick: fix indentation

--- src/redux/actions/loginActions.js
+++ src/redux/actions/loginActions.js
@@ -1,5 +1,4 @@
 import { createAction } from 'redux-actions';
-import * as actions from './actionTypes'
 import auth from '../../api/authentication'
 
 /*
@@ -8,42 +7,42 @@ import auth from '../../api/authentication'
  |--------------------------------------------------------------------------
  */
 
-const login = createAction(actions.LOGIN)
-const authSuccess = createAction(actions.LOGIN_SUCCESS)
+// const login = createAction(actions.LOGIN)
+// const authSuccess = createAction(actions.LOGIN_SUCCESS)
 
 const loginUser = (username, password) => {
-  let requestToken, sessionId, accountId
-   return async (dispatch) => {
+  // let requestToken, sessionId, accountId
+  // return async (dispatch) => {
 
-    try {
-      dispatch(login)
-      const requestTokenResponse = await auth.getRequestToken()
-      requestToken = requestTokenResponse.data.request_token  
-      dispatch(authSuccess)
-    } catch(error) {
-      console.log(error)
-    }
+  //   try {
+  //     dispatch(login)
+  //     const requestTokenResponse = await auth.getRequestToken()
+  //     requestToken = requestTokenResponse.data.request_token  
+  //     dispatch(authSuccess)
+  //   } catch(error) {
+  //     console.log(error)
+  //   }
 
-    try {
-     const loginResponse = await auth.login(username, password, requestToken)
-    } catch(error) {
-      console.log(error)
-    }
+  //   try {
+  //     const loginResponse = await auth.login(username, password, requestToken)
+  //   } catch(error) {
+  //     console.log(error)
+  //   }
     
-    try {
-     const sessionResponse = await auth.createSession(requestToken)  
-     sessionId = sessionResponse.data.session_id
-    } catch(error) {
-      console.log(error)
-    }
+  //   try {
+  //     const sessionResponse = await auth.createSession(requestToken)  
+  //     sessionId = sessionResponse.data.session_id
+  //   } catch(error) {
+  //     console.log(error)
+  //   }
 
-    try {
-      const userAccountResponse = await auth.getAccount(sessionId)
-      accountId = userAccountResponse.data.id
-    } catch(error) {
-      console.log(error)
-    }
-  }
+  //   try {
+  //     const userAccountResponse = await auth.getAccount(sessionId)
+  //     accountId = userAccountResponse.data.id
+  //   } catch(error) {
+  //     console.log(error)
+  //   }
+  // }
 }
 
 export default {
