3eb5c733069490b8083bf28908bed846b7d8c8e4
Puzant <pbakjejian@hotmail.com>
2021-03-09 11:51:28 +0200

    :zap: add new props to search reducer

--- src/redux/reducers/search.js
+++ src/redux/reducers/search.js
@@ -5,22 +5,25 @@ export const initialState = {
   searchResults: [],
   pending: false,
   error: false,
-  emptyResults: false
+  empty: false
 } 
 
 const fetchMovieByQuerySuccess = (state, {payload}) => ({
   ...state,
   searchResults: payload.searchResults.results,
   pending: false,
-  error: false
+  error: false,
+  empty: false
 })
 const fetchMovieByQueryPending = (state) => ({...state, pending: true, error: false})
 const fetchMOvieByQueryError = (state) => ({ ...state, error: true, pending: false })
+const emptyResults = (state) => ({...state, searchResults: [], pending: false, empty: true})
 
 const searchMovieActionsHandler = {
   [SEARCH_MOVIES_ACTIONS.SUCCESS]: fetchMovieByQuerySuccess,
   [SEARCH_MOVIES_ACTIONS.PENDING]: fetchMovieByQueryPending,
   [SEARCH_MOVIES_ACTIONS.ERROR]: fetchMOvieByQueryError,
+  [SEARCH_MOVIES_ACTIONS.EMPTY]: emptyResults,
 }
 
 export default handleActions(searchMovieActionsHandler, initialState)
