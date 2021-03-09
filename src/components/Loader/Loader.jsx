e519a1eda573b9fb7cf82d4f70596ddc727a9ada
Puzant <pbakjejian@hotmail.com>
2021-03-09 11:46:24 +0200

    :hammer: refactor to styled components

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import loader from '../../assets/loader-dotted.gif'
+import styled from 'styled-components'
 
 const Loader = ({pendingState}) => (
-  <div className="movie-loader-container">
-      {pendingState && <img className="movies-loader" alt="loading icon" src={loader} />}
-  </div>
+  <LoaderContainer>
+      {pendingState && <MoviesLoader alt="loading icon" src={loader} />}
+  </LoaderContainer>
 )
 
 export default Loader
+
+const LoaderContainer = styled.div`
+  margin: auto;
+`
+
+const MoviesLoader = styled.img`
+  width: 75px;
+  height: 100%;
+`
