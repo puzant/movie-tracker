9fbc666c7dd5f48d0d370fe48999315fb5fca2b8
Puzant <pbakjejian@hotmail.com>
2021-03-22 18:15:03 +0200

    :hammer: refactor and remove css

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,43 +1,39 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import React from 'react'
+import styled from 'styled-components'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
 import UnknowActorImage from '../../assets/unknown-actor.jpg'
 
-const useStyles = makeStyles({
-  root: {
-    width: '155px',
-    margin: '10px'
-  },
-  profileMedia: {
-    width: 155,
-    height: 235,
-  }
-});
-
-const Cast = (props) => {
-  const classes = useStyles();
-
+const Cast = ({actor}) => {
   return (
-    <Card className={classes.root} elevation={3}>
-      <CardMedia
-        className={classes.profileMedia}
-        image={props.actor.profile_path ? 
-              `https://image.tmdb.org/t/p/w185/${props.actor.profile_path}` 
-            : UnknowActorImage
-          }
-        title={props.actor.name}
+    <Root elevation={3}>
+      <ProfileImage
+        image={actor.profile_path ? 
+          `https://image.tmdb.org/t/p/w185/${actor.profile_path}` 
+          : UnknowActorImage
+        }
+        title={actor.name}
       />
-        <CardContent>
-          <Typography variant="h6">{props.actor.name}</Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {props.actor.character}
-          </Typography>
-        </CardContent>
-      </Card>
+      <CardContent>
+        <Typography variant="h6">{actor.name}</Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {actor.character}
+        </Typography>
+      </CardContent>
+    </Root>
   );
 }
 
 export default Cast
+
+const Root = styled(Card)`
+  width: 155px;
+  margin: 10px;
+`
+
+const ProfileImage = styled(CardMedia)`
+  width: 155px;
+  height: 235px; 
+`
