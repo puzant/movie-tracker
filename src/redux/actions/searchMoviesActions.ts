56bb5902d62bacfdc6ffbae1677b0427c6a0b495
Puzant <pbakjejian@hotmail.com>
2021-04-06 22:50:36 +0300

    change the file to Typescript :blue_car:

---
+++ src/redux/actions/searchMoviesActions.ts
@@ -0,0 +1,35 @@
+import { createAction } from 'redux-actions';
+import { SEARCH_MOVIES_ACTIONS } from '../actionTypes/searchMoviesActionTypes'
+import api from '../../api/api'
+import { IMovie } from '../../api/Models'
+
+/*
+ |--------------------------------------------------------------------------
+ | Get Movie By Query
+ |--------------------------------------------------------------------------
+ */
+
+const fetchMovieByQuerySuccess = createAction(SEARCH_MOVIES_ACTIONS.SUCCESS, (searchResults: IMovie[]) => ({searchResults}))
+const fetchMovieByQueryPending = createAction(SEARCH_MOVIES_ACTIONS.PENDING)
+const fetchMovieByQueryError = createAction(SEARCH_MOVIES_ACTIONS.ERROR, (error: any) => ({error}))
+const emptySearchResults = createAction(SEARCH_MOVIES_ACTIONS.EMPTY)
+
+const fetchMovieByQuery = (query: string) => {
+  return async (dispatch: any) => {
+    try {
+      dispatch(fetchMovieByQueryPending())
+      const movieByQueryResponse = await api.getMovieByQuery(query)
+      if (!movieByQueryResponse.data.results.length)
+        return dispatch(emptySearchResults())
+      
+      dispatch(fetchMovieByQuerySuccess(movieByQueryResponse.data))
+      
+    } catch(error) {
+      dispatch(fetchMovieByQueryError(error))
+    }
+  }
+}
+
+export default {
+  fetchMovieByQuery 
+}
