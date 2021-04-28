b375d01d642399067e0fc5d54540221f7d252c2f
Puzant <pbakjejian@hotmail.com>
2021-04-28 12:04:05 +0300

    change export type :zap:

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { App } from './App'
 import {Provider} from 'react-redux'
-import * as serviceWorker from './serviceWorker';
-import store from './redux/store';
+import * as serviceWorker from './serviceWorker'
+import store from './redux/store'
 
 const render = () => ReactDOM.render(
   <Provider store={store}>
