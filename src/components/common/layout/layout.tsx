3d1ddc5ff63212f938ae5451766801d13ccd234b
Puzant <pbakjejian@hotmail.com>
2021-05-30 11:35:30 +0300

    :zap: refactoring and general updates

--- src/components/common/layout/layout.tsx
+++ src/components/common/layout/layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react'
 import styled from 'styled-components'
-import Navbar from '../../navbar/navbar'
+import Navbar from '../../header/header'
 
 interface LayoutProps {
   children: ReactElement | ReactElement[]
