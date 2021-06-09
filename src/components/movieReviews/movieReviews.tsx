6296de9b63255db7b49301a7c239db847007422d
Puzant <pbakjejian@hotmail.com>
2021-06-09 20:34:45 +0300

    rename random color prop

--- src/components/movieReviews/movieReviews.tsx
+++ src/components/movieReviews/movieReviews.tsx
@@ -1,62 +1,67 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import Avatar from '@material-ui/core/Avatar'
-import { Review } from 'api/Models'
+import Avatar from "@material-ui/core/Avatar";
+import { Review } from "api/Models";
 
-import { Block } from 'components/common/block/block'
-import utils from 'utils/utils' 
+import { Block } from "components/common/block/block";
+import utils from "utils/utils";
 
 export interface MovieReviewsProps {
-  reviews: Review[]
-  avatarRandomColor: string
+  reviews: Review[];
 }
 
-const MovieReviews = ({reviews, avatarRandomColor}: MovieReviewsProps) => {
-
-  const [visible, setVisibility] = useState<string | null>(null)
+const MovieReviews = ({ reviews }: MovieReviewsProps) => {
+  const [visible, setVisibility] = useState<string | null>(null);
+  const randomColor: string = utils.generateRandomColorValue();
 
   return (
     <Root>
-
       <MovieReviewsTitle>Movie Reviews</MovieReviewsTitle>
 
-      {!reviews.length &&
+      {!reviews.length && (
         <NoMovieReviewText>
           There are no reviews for this movie
         </NoMovieReviewText>
-      }
+      )}
 
       <MovieReviewContainer>
-      {
-        reviews.map((review: Review) => (
+        {reviews.map((review: Review) => (
           <ReviewBox key={review.id}>
             <UserContainer>
-              <UserAvatar avatarRandomColor={avatarRandomColor}>{review.author[0]}</UserAvatar>
-              <Block align='flex-start' gap={6}>
+              <UserAvatar avatarColor={randomColor}>
+                {review.author[0]}
+              </UserAvatar>
+              <Block align="flex-start" gap={6}>
                 <UserName>{review.author}</UserName>
-                <span>Written by {review.author} on {utils.formatReviewDate(review.created_at)}</span>
+                <span>
+                  Written by {review.author} on{" "}
+                  {utils.formatReviewDate(review.created_at)}
+                </span>
               </Block>
             </UserContainer>
             <ReviewContent>
               <>
-                {visible === review.id ? review.content : `${review.content.substr(0, 250).trim()} ...`}
-                {
-                visible === review.id ? 
-                  <ReviewButton onClick={() => setVisibility(null)}>read less</ReviewButton>
-                  :
-                  <ReviewButton onClick={() => setVisibility(review.id)}>read more</ReviewButton>
-                }
+                {visible === review.id
+                  ? review.content
+                  : `${review.content.substr(0, 250).trim()} ...`}
+                {visible === review.id ? (
+                  <ReviewButton onClick={() => setVisibility(null)}>
+                    read less
+                  </ReviewButton>
+                ) : (
+                  <ReviewButton onClick={() => setVisibility(review.id)}>
+                    read more
+                  </ReviewButton>
+                )}
               </>
             </ReviewContent>
           </ReviewBox>
-        ))
-      }
+        ))}
       </MovieReviewContainer>
-
     </Root>
-  )
-}
+  );
+};
 
 const Root = styled.div`
   display: flex;
@@ -64,53 +69,53 @@ const Root = styled.div`
   justify-content: center;
   margin: auto;
   width: 83%;
-`
+`;
 
 const MovieReviewContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-`
+`;
 
 const ReviewBox = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: 5px;
   width: auto;
-  box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2), 0px 3px 4px 0px rgba(0,0,0,0.14), 0px 1px 8px 0px rgba(0,0,0,0.12);
+  box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2),
+    0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12);
   padding: 20px;
   margin: 10px auto 10px 0;
   background-color: #fafafa;
-`
+`;
 
 const UserContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   gap: 8px;
-`
+`;
 
 const UserName = styled.div`
   font-weight: bold;
   font-size: 20px;
-`
+`;
 
-const UserAvatar = styled(Avatar)
-<{
-  avatarRandomColor: string
+const UserAvatar = styled(Avatar)<{
+  avatarColor: string;
 }>`
-  background-color: ${props => `#${props.avatarRandomColor} !important`};
-`
+  background-color: ${(props) => `#${props.avatarColor} !important`};
+`;
 
 const ReviewButton = styled.a`
   color: #4ebdb6;
   text-decoration: underLine;
   cursor: pointer;
-`
+`;
 
 const ReviewContent = styled.div`
   margin-top: 10px;
   line-height: 25px;
-`
+`;
 
 const MovieReviewsTitle = styled.div`
   font-size: 24px;
@@ -118,11 +123,11 @@ const MovieReviewsTitle = styled.div`
   margin-bottom: 14px;
   align-self: center;
   text-align: center;
-`
+`;
 
 const NoMovieReviewText = styled.span`
   text-align: center;
   font-size: 20px;
-`
+`;
 
-export default MovieReviews
+export default MovieReviews;
--- src/pages/movieOverview/movieOverview.tsx
+++ src/pages/movieOverview/movieOverview.tsx
@@ -1,38 +1,38 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components'
-import { Link, useParams } from 'react-router-dom'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import movieOverviewActions from 'redux/actions/movieOverviewActions'
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { Link, useParams } from "react-router-dom";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import movieOverviewActions from "redux/actions/movieOverviewActions";
 
-import { IMovie, Review, Actor, MovieGenre } from 'api/Models'
+import { IMovie, Review, Actor, MovieGenre } from "api/Models";
 
-import { makeStyles } from '@material-ui/core/styles'
-import Box from '@material-ui/core/Box'
-import Tooltip from '@material-ui/core/Tooltip'
-import Fab from '@material-ui/core/Fab'
-import Rating from '@material-ui/lab/Rating'
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Tooltip from "@material-ui/core/Tooltip";
+import Fab from "@material-ui/core/Fab";
+import Rating from "@material-ui/lab/Rating";
 
-import MovieReviews from 'components/movieReviews/movieReviews'
-import Loader from 'components/loader/loader'
-import Constants from 'constants/Constants'
-import Cast from 'components/cast/cast'
-import Movie from 'components/movie/movie'
-import { Block, BlockGroup } from 'components/common/block/block'
+import MovieReviews from "components/movieReviews/movieReviews";
+import Loader from "components/loader/loader";
+import Constants from "constants/Constants";
+import Cast from "components/cast/cast";
+import Movie from "components/movie/movie";
+import { Block, BlockGroup } from "components/common/block/block";
 
-import utils from 'utils/utils' 
+import utils from "utils/utils";
 
 interface MovieInfoProp {
-  text: string 
-  value: string
+  text: string;
+  value: string;
 }
 
 export interface MovieOverviewProps {
-  fetchMovieById: (movieId: string) => void
-  fetchMovieReviews: (movieId: string) => void
-  movie: IMovie
-  pending: boolean
-  movieReviews: Review[]
+  fetchMovieById: (movieId: string) => void;
+  fetchMovieReviews: (movieId: string) => void;
+  movie: IMovie;
+  pending: boolean;
+  movieReviews: Review[];
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -41,130 +41,138 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const MovieInfo = ({text, value}: MovieInfoProp) => (
+const MovieInfo = ({ text, value }: MovieInfoProp) => (
   <MovieInfoContainer>
     <MovieInfoText>{text}: </MovieInfoText>
     <span>{value}</span>
   </MovieInfoContainer>
-)
+);
 
-const RenderMovieRunTime = ({runtime}: {runtime: number}) => {
+const RenderMovieRunTime = ({ runtime }: { runtime: number }) => {
   return (
     <MovieInfoContainer>
       <MovieInfoText>Run Time: </MovieInfoText>
-      <span>{ `${Math.floor(runtime / 60)}h ${Math.floor(runtime % 60)}m` }</span>
+      <span>{`${Math.floor(runtime / 60)}h ${Math.floor(runtime % 60)}m`}</span>
     </MovieInfoContainer>
-  )
-}
+  );
+};
 
 export const MovieOverview = ({
   fetchMovieById,
   fetchMovieReviews,
   movie,
   pending,
-  movieReviews
+  movieReviews,
 }: MovieOverviewProps) => {
-
   const classes = useStyles();
-  
-  const { movieId } = useParams<{ movieId: string }>()
-  const MAX_NUMBER_OF_ACTORS: number = 7
-  const actors: Actor[] = movie.credits?.cast?.slice(0, MAX_NUMBER_OF_ACTORS) ?? [];
-  const randomColor: string = utils.generateRandomColorValue()
-  
+
+  const { movieId } = useParams<{ movieId: string }>();
+  const MAX_NUMBER_OF_ACTORS: number = 7;
+  const actors: Actor[] =
+    movie.credits?.cast?.slice(0, MAX_NUMBER_OF_ACTORS) ?? [];
+
   useEffect(() => {
-    fetchMovieById(movieId)
-    fetchMovieReviews(movieId)
-  }, [])
+    fetchMovieById(movieId);
+    fetchMovieReviews(movieId);
+  }, []);
 
-  return ( 
-    <BlockGroup layout='vertical' justify='center'>
-
-      {!pending && 
+  return (
+    <BlockGroup layout="vertical" justify="center">
+      {!pending && (
         <MovieOverviewContainer backdropPath={movie.backdrop_path}>
-      
-        <MoviePosterContainer>
-          <MoviePoster src={"http://image.tmdb.org/t/p/w342/" + movie.poster_path} alt={movie.poster_path}/>
-        </MoviePosterContainer>
+          <MoviePosterContainer>
+            <MoviePoster
+              src={"http://image.tmdb.org/t/p/w342/" + movie.poster_path}
+              alt={movie.poster_path}
+            />
+          </MoviePosterContainer>
 
-        <MovieDescriptionContainer>
-
-          <BlockGroup>
-
-            <MovieInfoContainer>
-              <MovieTitle>{movie.title}</MovieTitle>&nbsp;
-              <MovieReleaseDate>({movie.release_date})</MovieReleaseDate>
-            </MovieInfoContainer>
-            
-            <Block gap={10}>
-              <MovieTagLine>{movie.tagline}</MovieTagLine>
+          <MovieDescriptionContainer>
+            <BlockGroup>
               <MovieInfoContainer>
-                <Rating
-                  classes={{ iconEmpty: classes.iconEmpty }}
-                  style={{marginTop: '5px'}}
-                  value={Number(movie.vote_average) / 2}
-                  precision={0.5}
-                  readOnly
-                />
-                <Box 
-                  component='span' 
-                  ml={0.75} 
-                  fontWeight="fontWeightMedium" 
-                  fontSize="h6.fontSize"
-                >
-                  {Number(movie.vote_average) / 2}
-                </Box>
+                <MovieTitle>{movie.title}</MovieTitle>&nbsp;
+                <MovieReleaseDate>({movie.release_date})</MovieReleaseDate>
               </MovieInfoContainer>
-              
-              <MovieInfo text="Status" value={movie.status} />
-              <MovieInfo text="Genres" value={movie?.genres?.map((genre: MovieGenre) => genre.name).join(', ')}></MovieInfo>
-              <MovieInfo text="Language" value={utils.getMovieLanguage(movie.original_language)} />
-              <RenderMovieRunTime runtime={movie.runtime} />
-              <MovieInfo text="About The Movie" value={movie.overview} />
-            </Block>
 
-          </BlockGroup>
+              <Block gap={10}>
+                <MovieTagLine>{movie.tagline}</MovieTagLine>
+                <MovieInfoContainer>
+                  <Rating
+                    classes={{ iconEmpty: classes.iconEmpty }}
+                    style={{ marginTop: "5px" }}
+                    value={Number(movie.vote_average) / 2}
+                    precision={0.5}
+                    readOnly
+                  />
+                  <Box
+                    component="span"
+                    ml={0.75}
+                    fontWeight="fontWeightMedium"
+                    fontSize="h6.fontSize"
+                  >
+                    {Number(movie.vote_average) / 2}
+                  </Box>
+                </MovieInfoContainer>
 
-          <MovieActionsConatiner layout='horizontal' gap={10}>
-            <Tooltip title="login to add to your favorite list" aria-label="add">
-              <StyledFab color="primary">
-                {Constants.MOVIE_OVERVIEW_USER_ACTIONS_ICONS.FAVORITE_MOVIE}
-              </StyledFab>
-            </Tooltip>
+                <MovieInfo text="Status" value={movie.status} />
+                <MovieInfo
+                  text="Genres"
+                  value={movie?.genres
+                    ?.map((genre: MovieGenre) => genre.name)
+                    .join(", ")}
+                ></MovieInfo>
+                <MovieInfo
+                  text="Language"
+                  value={utils.getMovieLanguage(movie.original_language)}
+                />
+                <RenderMovieRunTime runtime={movie.runtime} />
+                <MovieInfo text="About The Movie" value={movie.overview} />
+              </Block>
+            </BlockGroup>
 
-            <Tooltip title="login to add to your watchlist" aria-label="add">
-              <StyledFab color="primary">
-                {Constants.MOVIE_OVERVIEW_USER_ACTIONS_ICONS.WATCHLIST_MOVIE}
-              </StyledFab>
-            </Tooltip>
+            <MovieActionsConatiner layout="horizontal" gap={10}>
+              <Tooltip
+                title="login to add to your favorite list"
+                aria-label="add"
+              >
+                <StyledFab color="primary">
+                  {Constants.MOVIE_OVERVIEW_USER_ACTIONS_ICONS.FAVORITE_MOVIE}
+                </StyledFab>
+              </Tooltip>
 
-            <Tooltip title="login to add to rate this movie" aria-label="add">
-              <StyledFab color="primary">
-                {Constants.MOVIE_OVERVIEW_USER_ACTIONS_ICONS.RATE_MOVIE}
-              </StyledFab>
-            </Tooltip>
-          </MovieActionsConatiner>
+              <Tooltip title="login to add to your watchlist" aria-label="add">
+                <StyledFab color="primary">
+                  {Constants.MOVIE_OVERVIEW_USER_ACTIONS_ICONS.WATCHLIST_MOVIE}
+                </StyledFab>
+              </Tooltip>
 
-        </MovieDescriptionContainer>
-
-      </MovieOverviewContainer>}
+              <Tooltip title="login to add to rate this movie" aria-label="add">
+                <StyledFab color="primary">
+                  {Constants.MOVIE_OVERVIEW_USER_ACTIONS_ICONS.RATE_MOVIE}
+                </StyledFab>
+              </Tooltip>
+            </MovieActionsConatiner>
+          </MovieDescriptionContainer>
+        </MovieOverviewContainer>
+      )}
 
       <Loader pendingState={pending} />
 
-      {!pending && 
+      {!pending && (
         <BlockGroup>
           <SectionTitle>Top Cast</SectionTitle>
-          <Block layout='horizontal' justify='center' wrapped>
-            {movie.credits?.cast?.length > 0 && actors.map((actor: Actor) => (
-              <Cast key={actor.cast_id} actor={actor} />
-            ))}
+          <Block layout="horizontal" justify="center" wrapped>
+            {movie.credits?.cast?.length > 0 &&
+              actors.map((actor: Actor) => (
+                <Cast key={actor.cast_id} actor={actor} />
+              ))}
           </Block>
         </BlockGroup>
-      }
+      )}
 
-      {!pending && <MovieReviews avatarRandomColor={randomColor} reviews={movieReviews} />}
+      {!pending && <MovieReviews reviews={movieReviews} />}
 
-      {!pending && 
+      {!pending && (
         <Block>
           <SectionTitle>Recommendations</SectionTitle>
           <RecommendationsList>
@@ -175,58 +183,55 @@ export const MovieOverview = ({
             ))}
           </RecommendationsList>
         </Block>
-      }
-
+      )}
     </BlockGroup>
-
   );
-}
+};
 
-
-const mapStateToProps = (state: any) => { 
+const mapStateToProps = (state: any) => {
   return {
     movie: state.movie.movie,
     pending: state.movie.pending,
     error: state.movie.error,
-    movieReviews: state.movie.movieReviews
-  }
-}
+    movieReviews: state.movie.movieReviews,
+  };
+};
 
 const mapDispatchToProps = (dispatch: any) => {
-  return bindActionCreators(movieOverviewActions, dispatch)
-}
+  return bindActionCreators(movieOverviewActions, dispatch);
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(MovieOverview)
+export default connect(mapStateToProps, mapDispatchToProps)(MovieOverview);
 
 const SectionTitle = styled.div`
   align-self: center;
   font-size: 24px;
   margin: 20px 0 20px 0;
-`
+`;
 
 const RecommendationsList = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-`
+`;
 
 const MovieTitle = styled.span`
   font-size: 30px;
   font-weight: bold;
-`
+`;
 
 const MovieReleaseDate = styled.span`
   margin-top: 4px;
-  opacity: .8;
-`
+  opacity: 0.8;
+`;
 
 const MovieTagLine = styled.div`
   font-style: italic;
   margin-top: 3px;
-`
+`;
 
 const MovieOverviewContainer = styled.div<{
-  backdropPath: string
+  backdropPath: string;
 }>`
   display: flex;
   align-items: center;
@@ -234,7 +239,8 @@ const MovieOverviewContainer = styled.div<{
   background-color: #fff;
   color: #111;
   box-shadow: -2px 4px 30px -1px #000000bf;
-  background: ${props => `url(https://image.tmdb.org/t/p/w1280/${props.backdropPath})`};
+  background: ${(props) =>
+    `url(https://image.tmdb.org/t/p/w1280/${props.backdropPath})`};
   background-size: cover;
   background-position: center, center;
   box-shadow: inset 0 0 0 100vw rgb(0 0 0 / 70%);
@@ -249,16 +255,16 @@ const MovieOverviewContainer = styled.div<{
     margin: auto;
     border-radius: 0px;
   }
-`
+`;
 
 const MovieInfoContainer = styled.div`
   /* margin-top: 10px; */
-`
+`;
 
 const MovieInfoText = styled.span`
   color: #a0d2eb;
   font-weight: bold;
-`
+`;
 
 const MoviePosterContainer = styled.div`
   @media (max-width: 600px) {
@@ -266,7 +272,7 @@ const MoviePosterContainer = styled.div`
     display: flex;
     justify-content: center;
   }
-`
+`;
 
 const MoviePoster = styled.img`
   height: 450px;
@@ -276,7 +282,7 @@ const MoviePoster = styled.img`
     height: 280px;
     width: 180px;
   }
-`
+`;
 
 const MovieDescriptionContainer = styled.div`
   margin-left: 20px;
@@ -284,16 +290,16 @@ const MovieDescriptionContainer = styled.div`
   @media (max-width: 600px) {
     text-align: center;
   }
-`
+`;
 
 const StyledLink = styled(Link)`
   color: #111;
   text-decoration: none;
-`
+`;
 
 const StyledFab = styled(Fab)`
-background-color: #418baf !important;
-`
+  background-color: #418baf !important;
+`;
 
 const MovieActionsConatiner = styled(Block)`
   margin-top: 10px;
@@ -301,4 +307,4 @@ const MovieActionsConatiner = styled(Block)`
     display: flex;
     justify-content: center;
   }
-`
+`;
