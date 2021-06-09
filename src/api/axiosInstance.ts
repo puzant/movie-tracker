a36f7608eb6afaf3292a7a101eff993babf11968
Puzant <pbakjejian@hotmail.com>
2021-06-09 12:39:07 +0300

    test pre-commit hook

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,24 +1,30 @@
-import axios from 'axios'
+import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_TMDB_API_BASE_URL,
 });
 
 //  attach the API_KEY with every request
-axiosInstance.interceptors.request.use((config: any) => {
-  config.params = {
-    ...config.params,
-    api_key: process.env.REACT_APP_API_KEY
+axiosInstance.interceptors.request.use(
+  (config: any) => {
+    config.params = {
+      ...config.params,
+      api_key: process.env.REACT_APP_API_KEY,
+    };
+    return config;
+  },
+  (error: any) => {
+    return Promise.reject(error);
   }
-  return config
-}, (error: any) => {
-  return Promise.reject(error)
-})
+);
 
-axiosInstance.interceptors.response.use((response: any) => {
-  return response
-}, (error: any) => {
-  return Promise.reject(error)
-})
+axiosInstance.interceptors.response.use(
+  (response: any) => {
+    return response;
+  },
+  (error: any) => {
+    return Promise.reject(error);
+  }
+);
 
-export default axiosInstance
+export default axiosInstance;
