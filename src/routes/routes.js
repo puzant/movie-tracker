3e21faab4a4ea934513cb7ea93a5c23506d85164
Puzant <pbakjejian@hotmail.com>
2021-06-20 13:45:39 +0300

    import named exports & use named aliases for using it

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import { useDispatch, useSelector } from "react-redux"
-import { Switch, Route } from "react-router-dom"
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Switch, Route } from "react-router-dom";
 
-import DiscoverMovies from '../pages/discoverMovies/discoverMovies'
-import MovieOverview from '../pages/movieOverview/movieOverview'
-import MoviesSearch from '../pages/moviesSearch/moviesSearch'
-import UpcomingMovies from '../pages/upcomingMovies/upcomingMovies'
-import Login from '../pages/login/login'
-import NotFound from '../pages/notFound/notFound'
+import {
+  DiscoverMoviesConnected as DiscoverMovies,
+  MovieOverviewConnected as MovieOverview,
+  MoviesSearchConnected as MoviesSearch,
+  UpcomingMoviesConnected as UpcomingMovies,
+  LoginConnected as Login,
+  NotFound,
+} from "pages";
 
-import GuardedRoute from './guardedRoute'
+import GuardedRoute from "./guardedRoute";
 
-const notFoundPage = () => <NotFound errorText="Page Not Found"/>
+const notFoundPage = () => <NotFound errorText="Page Not Found" />;
 
 const Routes = () => {
-
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   return (
     <Switch>
@@ -23,10 +24,10 @@ const Routes = () => {
       <Route path="/movie-overview/:movieId" exact component={MovieOverview} />
       <Route path="/search-results" exact component={MoviesSearch} />
       <Route path="/upcoming-movies" exact component={UpcomingMovies} />
-      <Route path ="/login" exact component={Login} />
+      <Route path="/login" exact component={Login} />
       <Route component={notFoundPage} />
     </Switch>
-  )
-}
+  );
+};
 
-export default Routes
+export default Routes;
