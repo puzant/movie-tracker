575e981d473aa5964f078e05d749e127e4a5bc5e
Puzant <pbakjejian@hotmail.com>
2021-06-20 13:45:16 +0300

    export everything from pages

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export * from "./discoverMovies/discoverMovies";
+export * from "./movieOverview/movieOverview";
+export * from "./moviesSearch/moviesSearch";
+export * from "./notFound/notFound";
+export * from "./upcomingMovies/upcomingMovies";
+export * from "./login/login";
